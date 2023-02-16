1cbafb8cd291ae71f805a3563b38defdfba0c7a6
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-02-15 22:39:29 -0500

    created task model

--- taskmaster/app/src/main/java/com/devonterry/taskmaster/Task.java
+++ taskmaster/app/src/main/java/com/devonterry/taskmaster/Task.java
@@ -1,4 +1,27 @@
 package com.devonterry.taskmaster;
 
 public class Task {
+    private String taskName;
+    private String taskDescription;
+
+    public Task(String taskName, String taskDescription){
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(){
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(){
+        this.taskDescription = taskDescription;
+    }
 }
