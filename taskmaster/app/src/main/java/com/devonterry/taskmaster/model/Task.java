780732707705c5cec546394b90ff795c4aaa673e
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-02-16 12:25:54 -0500

    completed recycler view

--- taskmaster/app/src/main/java/com/devonterry/taskmaster/model/Task.java
+++ taskmaster/app/src/main/java/com/devonterry/taskmaster/model/Task.java
@@ -3,27 +3,33 @@ package com.devonterry.taskmaster.model;
 import java.util.List;
 
 public class Task {
-    private String taskName;
-    private String body;
+    private String taskTitle;
+    private String taskBody;
+    private String taskState;
 
-    public Task(String taskName, String body){
-        this.taskName = taskName;
-        this.body = body;
+    public Task(String taskTitle, String taskBody, String taskState){
+        this.taskTitle = taskTitle;
+        this.taskBody = taskBody;
+        this.taskState = taskState;
     }
 
-    public String getTaskName() {
-        return taskName;
+    public String getTaskTitle() {
+        return taskTitle;
     }
 
-    public void setTaskName(){
-        this.taskName = taskName;
+    public void setTaskTitle(){
+        this.taskTitle = taskTitle;
     }
 
-    public String getBody() {
-        return body;
+    public String getTaskBody() {
+        return taskBody;
     }
 
-    public void setBody(){
-        this.body = body;
+    public void setTaskBody(){
+        this.taskBody = taskBody;
     }
+
+    public String getTaskState(){ return taskState; }
+
+    public void setTaskState() { this.taskState = taskState; }
 }
