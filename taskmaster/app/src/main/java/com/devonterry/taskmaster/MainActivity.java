470b193f59662250a8db3cb43eb9e872bfbd634a
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-02-15 22:05:53 -0500

    main buttons intent to task detail activity. main cosmetic change

--- taskmaster/app/src/main/java/com/devonterry/taskmaster/MainActivity.java
+++ taskmaster/app/src/main/java/com/devonterry/taskmaster/MainActivity.java
@@ -1,13 +1,21 @@
 package com.devonterry.taskmaster;
 
+import static com.devonterry.taskmaster.SettingsActivity.USER_USERNAME_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+//import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TASK_ADD_EXTRA_TAG = "taskDetail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,38 @@ public class MainActivity extends AppCompatActivity {
             startActivity(goToSettingsIntent);
         });
 
+        TextView taskDetailButtonOne = (TextView) findViewById(R.id.MainActivityBudgetButton);
+        taskDetailButtonOne.setOnClickListener(v -> {
+            String taskDetail = taskDetailButtonOne.getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASK_ADD_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
 
+        TextView taskDetailButtonTwo = (TextView) findViewById(R.id.MainActivityLearnFrenchButton);
+        taskDetailButtonTwo.setOnClickListener(v -> {
+            String taskDetail = taskDetailButtonTwo.getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASK_ADD_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
+
+        TextView taskDetailButtonThree = (TextView) findViewById(R.id.MainActivityWatchYouPeopleButton);
+        taskDetailButtonThree.setOnClickListener(v -> {
+            String taskDetail = taskDetailButtonThree.getText().toString();
+            Intent goToTaskDetailIntent = new Intent(this, TaskDetailActivity.class);
+            goToTaskDetailIntent.putExtra(TASK_ADD_EXTRA_TAG, taskDetail);
+            startActivity(goToTaskDetailIntent);
+        });
+
+    }
+
+    @Override
+    protected void  onResume() {
+
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String userUserName = preferences.getString(USER_USERNAME_TAG, "No Username");
+        ((TextView)findViewById(R.id.MainActivityUserNameTextView)).setText(userUserName + "'s Tasks");
     }
 }
--- taskmaster/app/src/main/java/com/devonterry/taskmaster/SettingsActivity.java
+++ taskmaster/app/src/main/java/com/devonterry/taskmaster/SettingsActivity.java
@@ -2,13 +2,42 @@ package com.devonterry.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    public static final String USER_USERNAME_TAG = "userUsername";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String userName = preferences.getString(USER_USERNAME_TAG, "No name found");
+        EditText userUserNameEditText = (EditText) findViewById(R.id.SettingsActivityNameInputTextView);
+        userUserNameEditText.setText(userName);
+
+
+        Button saveButton = findViewById(R.id.SettingsActivitySaveButton);
+        saveButton.setOnClickListener(v -> {
+            SharedPreferences.Editor preferencesEditor = preferences.edit();
+        String userUsernameString = userUserNameEditText.getText().toString();
+
+        preferencesEditor.putString(USER_USERNAME_TAG, userUsernameString);
+        preferencesEditor.apply();
+
+        Snackbar.make(findViewById(R.id.settingsActivity), "Settings Saved", Snackbar.LENGTH_SHORT).show();
+
+
+        });
     }
 }
